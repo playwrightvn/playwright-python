00c98e69599b79ef3894520f5f1cf1cecdee9bc7
K4 - Phong Do <dominhphong306@gmail.com>
2024-07-26 09:19:27 +0700

    feat: modify notes

--- lesson-2.py
+++ lesson-2.py
@@ -2,43 +2,30 @@ from playwright.sync_api import sync_playwright
 
 playwright = sync_playwright().start()
 browser = playwright.chromium.launch(headless=False)
+
 page = browser.new_page()
 
-# selectors
 page.goto('https://material.playwrightvn.com/01-xpath-register-page.html')
 
-# by xpath
+
+# Locator
+# Xpath
 username_loc = page.locator('//input[@id="username"]')
 username_loc.highlight()
 
-# by css selector
-email_loc = page.locator('#email')
+# CSS 
+email_loc = page.locator('//input[@id="email"]')
 email_loc.highlight()
 
-# by playwright selector
-submit_btn = page.get_by_role('button', name="Register")
-submit_btn.highlight()
+# Playwright locator
+register_loc = page.get_by_role("button", name="Register")
+register_loc.highlight()
 
-# mouse actions: click, dbclick, click w modifier
+# Action
+
+# click
 page.goto('https://material.playwrightvn.com/018-mouse.html')
-click_area = page.locator('#clickArea')
-click_area.highlight()
+click_area = page.locator('//input[@id="clickArea"]')
 click_area.click()
 click_area.dblclick()
-click_area.click(modifiers=['Shift'])
-
-# input action: fill, clear, type
-page.goto('https://material.playwrightvn.com/01-xpath-register-page.html')
-username_loc = page.locator('//input[@id="username"]')
-username_loc.highlight()
-
-username_loc.fill('Playwright Viet Nam')
-username_loc.clear()
-username_loc.type('Playwright Viet Nam', delay=2000)
-
-# checkbox action: check, uncheck, setchecked, click()
-
-
-# select option
-
-# input: set_input_files
+click_area.click(modifiers=[])
